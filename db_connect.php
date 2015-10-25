966b0ca6f2844ecdfff9a0b88e636e9a98bfad7e
justinduncan2010 <justinduncan2010@gmail.com>
2015-10-25 11:54:38 -0400

    github sync

--- db_connect.php
+++ db_connect.php
@@ -1,16 +1,10 @@
 <?php
 $mysqli = new mysqli('localhost', 'root', 'Jade7369!', 'ju655443');  
 
-<<<<<<< HEAD
 // Check connection
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
-=======
 mysql_select_db("ju655443",$con);
-<<<<<<< HEAD
->>>>>>> parent of 550560d... pagination working
-=======
->>>>>>> parent of 550560d... pagination working
 ?>
