36347ad06b065db9adca228cc04cd79e31eba041
justinduncan2010 <justinduncan2010@gmail.com>
2015-11-05 16:37:45 -0500

    logout

--- logout.php
+++ logout.php
@@ -1,7 +1,6 @@
 <?php
-$_SESSION = array();
-setcookie( session_name(), '', time()-300);
 
+session_start();
 if(session_destroy()) // Destroying All Sessions
 {
 header('Location: ' . $_SERVER['HTTP_REFERER']); // Redirecting To logout page
